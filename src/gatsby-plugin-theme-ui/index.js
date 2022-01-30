c3adaee5dd04045dbc191ab95a2c3947d76cab1a
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-01-29 21:22:13 -0300

    Added colors and reset styles to theme

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,7 +1,15 @@
 const theme = {
   colors: {
-    text: "#000",
-    background: "#fff",
+    black: "#0B0D17",
+    lila: "#D0D6F9",
+    white: "#ffffff",
+  },
+  styles: {
+    root: {
+      boxSizing: 'border-box',
+      padding: 0,
+      margin: 0
+    },
   },
 };
 
