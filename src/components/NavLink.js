3da3f709fc9f7a12e453f3a2a4198a149937a7cb
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-03-11 19:09:05 -0300

    Added NavLink component

---
+++ src/components/NavLink.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Link } from "theme-ui";
+
+export const NavLink = ({ children, href, sx }) => (
+  <Link
+    href={href}
+    sx={{
+      fontFamily: "body",
+      fontSize: 1,
+      letterSpacing: 1,
+      textTransform: "uppercase",
+      ...sx,
+    }}
+  >
+    {children}
+  </Link>
+);
