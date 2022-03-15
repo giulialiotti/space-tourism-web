8cf0947cdcf67ae77a246e86c2a8d72e4eb2c820
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-03-15 12:42:43 -0300

    Added gatsby-node.js to allow direct import paths

---
+++ gatsby-node.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        assets: path.resolve(__dirname, "src/assets"),
+        sections: path.resolve(__dirname, "src/sections"),
+        layouts: path.resolve(__dirname, "src/layouts"),
+      },
+    },
+  });
+};
