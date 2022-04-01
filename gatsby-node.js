486f0a579bcc53202d89e9c1814cea90ab2909c3
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-04-01 20:34:51 -0300

    Added hooks folder as direct import path

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,6 +8,7 @@ exports.onCreateWebpackConfig = ({ actions }) => {
         assets: path.resolve(__dirname, "src/assets"),
         sections: path.resolve(__dirname, "src/sections"),
         layouts: path.resolve(__dirname, "src/layouts"),
+        hooks: path.resolve(__dirname, "src/hooks"),
       },
     },
   });
