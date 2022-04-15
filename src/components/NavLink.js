ce4386360f32745bfd34cc9520f3f2bbdfb65df9
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-04-15 18:21:30 -0300

    NavLink: remove underline in mobile devices

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -3,39 +3,50 @@ import React from "react";
 // Local Components
 import { MotionLink, MotionBox } from "./Motion";
 
+// Hooks
+import { useMediaQuery } from "hooks/useMediaQuery";
+
 // Animations
 import { showLineOnHover } from "../sections/animations";
 
-export const NavLink = ({ children, href, sx }) => (
-  <MotionLink
-    href={href}
-    initial="default"
-    whileHover="hover"
-    animate="default"
-    sx={{
-      display: "flex",
-      color: "white",
-      fontFamily: "body",
-      fontSize: [16, 14, 16],
-      letterSpacing: 1,
-      textTransform: "uppercase",
-      textDecoration: "none",
-      ...sx,
-    }}
-  >
-    {children}
-    <MotionBox
-      as="span"
-      variants={showLineOnHover}
-      className="nav-link__underline"
+export const NavLink = ({ children, href, sx }) => {
+  const isBiggerDevice = useMediaQuery("(min-width: 750px)");
+
+  return (
+    <MotionLink
+      href={href}
+      initial="default"
+      whileHover="hover"
+      animate="default"
       sx={{
-        display: ["none", "inline-block", "inline-block"],
-        position: "absolute",
-        bg: "white",
-        bottom: 0,
-        left: 0,
-        height: "3px",
+        display: "flex",
+        color: "white",
+        fontFamily: "body",
+        fontSize: [16, 14, 16],
+        letterSpacing: 1,
+        textTransform: "uppercase",
+        textDecoration: "none",
+        ...sx,
       }}
-    />
-  </MotionLink>
+    >
+      {children}
+      {isBiggerDevice && <Underline variants={showLineOnHover} />}
+    </MotionLink>
+  );
+};
+
+const Underline = ({ sx, ...props }) => (
+  <MotionBox
+    as="span"
+    className="nav-link__underline"
+    sx={{
+      position: "absolute",
+      bg: "white",
+      bottom: 0,
+      left: 0,
+      height: "3px",
+      sx,
+    }}
+    {...props}
+  />
 );
