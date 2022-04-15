6c60ce868230d8cdf52b7f7f7c34bb6882136040
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-04-15 17:54:59 -0300

    Added Motion Components

--- src/components/Motion.js
+++ src/components/Motion.js
@@ -1,8 +1,10 @@
 import { motion } from "framer-motion";
-import { Box, Heading, Paragraph } from "theme-ui";
+import { Box, Heading, Paragraph, Button, Link } from "theme-ui";
 
 const MotionBox = motion(Box, { forwardMotionProps: true });
 const MotionHeading = motion(Heading, { forwardMotionProps: true });
 const MotionParagraph = motion(Paragraph, { forwardMotionProps: true });
+const MotionButton = motion(Button, { forwardMotionProps: true });
+const MotionLink = motion(Link, { forwardMotionProps: true });
 
-export { MotionBox, MotionHeading, MotionParagraph };
+export { MotionBox, MotionHeading, MotionParagraph, MotionButton, MotionLink };
