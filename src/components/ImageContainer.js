19e9bafc764189d62d3a157d7c898095b403875d
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-04-22 20:23:28 -0300

    Touch ups to ImageContainer component

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,12 +1,15 @@
 import React from "react";
+
+// External Components
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-import { Box } from "theme-ui";
+// Local Components
+import { MotionBox } from "./Motion";
 
 export const ImageContainer = ({ img, alt, sx, imgStyle, ...props }) => {
   return (
-    <Box className="gatsby-image-container" sx={{ ...sx }} {...props}>
+    <MotionBox className="gatsby-image-container" sx={{ ...sx }} {...props}>
       <GatsbyImage image={getImage(img)} alt={alt} imgStyle={{ ...imgStyle }} />
-    </Box>
+    </MotionBox>
   );
 };
