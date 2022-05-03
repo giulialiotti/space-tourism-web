c2cad03db55fc5d2d13fcfdcd769092be60673c7
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-05-03 15:58:48 -0300

    Fix link hover bug

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -4,39 +4,62 @@ import { jsx } from "theme-ui";
 // Local Components
 import { MotionLink, MotionBox } from "./Motion";
 
-export const NavLink = ({ children, href, pathname, sx }) => (
-  <MotionLink
-    bg="#0B0D17"
-    cover
-    direction="left"
-    to={href}
-    sx={{
-      display: "flex",
-      color: "white",
-      fontFamily: "body",
-      fontSize: [16, 14, 16],
-      letterSpacing: 1,
-      textTransform: "uppercase",
-      textDecoration: "none",
-      ...sx,
-    }}
-  >
-    {children}
-    {pathname === href && (
-      <Underline
-        sx={{
-          display: ["none", "inline-block", "inline-block"],
-          width: "100%",
-        }}
-      />
-    )}
-  </MotionLink>
-);
+// Hooks
+import { useMediaQuery } from "hooks/useMediaQuery";
 
-export const Underline = ({ sx, ...props }) => (
+export const NavLink = ({ children, href, pathname, sx, ...props }) => {
+  const isBiggerDevice = useMediaQuery("(min-width: 750px)");
+
+  return (
+    <MotionLink
+      bg="#0B0D17"
+      cover
+      direction="left"
+      to={href}
+      sx={{
+        display: "flex",
+        color: "white",
+        fontFamily: "body",
+        fontSize: [16, 14, 16],
+        letterSpacing: 1,
+        textTransform: "uppercase",
+        textDecoration: "none",
+        ...sx,
+        "&:hover .nav-link__underline-hover": {
+          transform: "scaleX(1)",
+        },
+      }}
+      {...props}
+    >
+      {children}
+      {isBiggerDevice && (
+        <Underline
+          className="nav-link__underline-hover"
+          sx={{
+            opacity: 0.5,
+            transform: "scaleX(0)",
+            transformOrigin: "left",
+            transition: "all .3s ease-in-out",
+            width: "100%",
+          }}
+        />
+      )}
+      {pathname === href && (
+        <Underline
+          className="nav-link__underline"
+          sx={{
+            display: ["none", "inline-block", "inline-block"],
+            width: "100%",
+          }}
+        />
+      )}
+    </MotionLink>
+  );
+};
+
+const Underline = ({ sx, ...props }) => (
   <MotionBox
     as="span"
-    className="nav-link__underline"
     sx={{
       position: "absolute",
       bg: "white",
